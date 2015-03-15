d6c7da16ae438a3eb4d8128592332ac491a48567
travestyalpha <travestyalpha@gmail.com>
2015-03-14 19:08:53 -0700

    Update AudioOSCTest.py

--- AudioOSCTest.py
+++ AudioOSCTest.py
@@ -4,73 +4,78 @@ from time import sleep
 import time
 import types
 import os
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import binascii
-import winsound
+import pygame
 
-server = OSCServer( ("192.168.1.70", 8080) )#This has to be the IP of the RaspberryPi on the network
+pygame.mixer.init()
+
+server = OSCServer( ("192.168.1.42", 8080) )#This has to be the IP of the RaspberryPi on the network
 #client = OSCClient()
 
 def handle_timeout(self):
-	print ("I'm IDLE")
-#This here is just to do something while the script recieves no information....
+	print ("waiting")
 server.handle_timeout = types.MethodType(handle_timeout, server)
  
-# BUTTONS
-####################################################################################################################################################
 def C(path, tags, args, source):
 	print "C"
-	winsound.Beep(261,250)
+        numnuts = pygame.mixer.Sound("numnuts.wav")
+        numnuts.play()
+        numnuts.set_volume(1.0)
 
 def Csharp(path, tags, args, source):
         print "C#"
-        winsound.Beep(277,250)
+        #winsound.Beep(277,250)
 
 def D(path, tags, args, source):
 	print "D"
-	winsound.Beep(294,250)
+	horse = pygame.mixer.Sound("0859.ogg")
+        horse.play()
+        horse.set_volume(1.0)
 
 def Dsharp(path, tags, args, source):
         print "D#"
-        winsound.Beep(311,250)
+        #winsound.Beep(311,250)
 
 def E(path, tags, args, source):
         print "E"
-        winsound.Beep(330,250)
+        wilhelm = pygame.mixer.Sound("0477.ogg")
+        wilhelm.play()
 
 def F(path, tags, args, source):
         print "F"
-        winsound.Beep(349,250)
+        #winsound.Beep(349,250)
 
 def Fsharp(path, tags, args, source):
         print "F#"
-        winsound.Beep(370,250)
+        #winsound.Beep(370,250)
 
 def G(path, tags, args, source):
         print "G"
-        winsound.Beep(392,250)
+        #winsound.Beep(392,250)
 
 def Gsharp(path, tags, args, source):
         print "G#"
-        winsound.Beep(415,250)
+        #winsound.Beep(415,250)
 
 def A(path, tags, args, source):
         print "A"
-        winsound.Beep(440,250)
+        #winsound.Beep(440,250)
 
 def Asharp(path, tags, args, source):
         print "A#"
-        winsound.Beep(466,250)
+        #winsound.Beep(466,250)
 
 def B(path, tags, args, source):
         print "B"
-        winsound.Beep(494,250)
+        #winsound.Beep(494,250)
 
 def CC(path, tags, args, source):
         print "CC"
-        winsound.Beep(523,250)
+        #winsound.Beep(523,250)
 
 #These are all the add-ons that you can name in the TouchOSC layout designer (you can set the values and directories)
+
 server.addMsgHandler("/C", C)
 server.addMsgHandler("/Csharp", Csharp)
 server.addMsgHandler("/D", D)
